85c1ee173637c6fb1d3caf3e292b1ebf2ea80ec1
Jared Peterson <jared.peterson@hey.com>
2021-01-03 12:28:16 -0500

    Adding two new utility functions and their corresponding unit tests.

--- file_utils/file_utils.py
+++ file_utils/file_utils.py
@@ -4,23 +4,23 @@
 Common file/directory utilities.
 
 Author: Jared Peterson
-Email: jared.peterson@gmail.com
+Email: jared.peterson@hey.com
 """
 
-from pathlib import Path
 from datetime import datetime
 from os import makedirs
 from os import path
+from pathlib import Path
 from shutil import copy
 from typing import List
 
 import pandas as pd
 
 
-def create_new_output_dir(prefix='./') -> str:
+def create_new_output_dir(prefix: str = './') -> str:
     """ Creates a new directory that output data can be written to.
 
-    :param prefix a string that will be concatenated with the generated name.
+    :param prefix a str that will be concatenated with the generated name.
     :return: a string containing the output directory name.
     """
 
@@ -32,10 +32,10 @@ def create_new_output_dir(prefix='./') -> str:
     return output_dir_path
 
 
-def list_filenames_in_dir(path_to_dir) -> List[str]:
+def list_filenames_in_dir(path_to_dir: str) -> List[str]:
     """ Return a list of only filenames within a directory.
 
-    :param path_to_dir: a string path to the directory.
+    :param path_to_dir: a str containing the path to the directory.
     :return: a list containing only the filenames in the directory.
     """
 
@@ -43,10 +43,10 @@ def list_filenames_in_dir(path_to_dir) -> List[str]:
     return list((str(file_or_dir) for file_or_dir in Path(path_to_dir).iterdir() if file_or_dir.is_file()))
 
 
-def copy_files_to_dir(path_to_dir, files_to_copy=None) -> None:
+def copy_files_to_dir(path_to_dir: str, files_to_copy=None) -> None:
     """ Copy a list of file paths to a destination directory.
 
-    :param path_to_dir: the path to the destination directory.
+    :param path_to_dir: a str containing the path to the destination directory.
     :param files_to_copy: a list containing the files to copy.
     """
 
@@ -61,7 +61,33 @@ def copy_files_to_dir(path_to_dir, files_to_copy=None) -> None:
         copy(file_to_copy, path_to_dir)
 
 
-def generate_csv_for_dir(path_to_documents_dir, path_to_csv_file, delimiter='|') -> None:
+def write_content_as_file(content: str, file_name: str, path_to_dir: str = '.') -> None:
+    """ This function will write the string content you pass it to the file specified.
+
+    :param content: the str content to write.
+    :param file_name: the name of the file to create and write the content to.
+    :param path_to_dir: a str containing the path of the directory where the file lives.
+    """
+
+    with open(Path(path_to_dir, file_name), 'w') as file_to_write:
+        file_to_write.write(content)
+
+
+def load_content_from_file(file_name: str, path_to_dir: str = '.') -> str:
+    """ This function will load the content of a file and return it as a string.
+
+    :param file_name: a str containing the name of the file to read.
+    :param path_to_dir: a str containing the path of the directory where the file lives.
+    :return: the content of the file as a str.
+    """
+
+    with open(Path(path_to_dir, file_name), 'r') as file:
+        content = file.read()
+
+    return content
+
+
+def generate_csv_for_dir(path_to_documents_dir: str, path_to_csv_file: str, delimiter: str = '|') -> None:
     """ This function will build a csv file where each row in the csv file represents a document in the directory.
 
     :param path_to_documents_dir: a string containing the path to the directory to use.
