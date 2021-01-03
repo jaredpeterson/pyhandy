6c7cb8b4d93955d8fb6f88199f20950366163dd1
Jared Peterson <jared.peterson@hey.com>
2021-01-03 14:05:58 -0500

    Updating file headers and basic formatting.

--- pyhandy/__init__.py
+++ pyhandy/__init__.py
@@ -1,2 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""PyHandy is a simple Python library for handling basic IO operations.
+"""
+
+__author__ = 'Jared Peterson'
+__license__ = 'apache-2.0'
+__maintainer__ = 'Jared Peterson'
+__email__ = 'jared.peterson@hey.com'
+__version__ = '0.0.1'
+__status__ = 'Development'
+
 # grab everything in file_utils.py
 from .file_utils import *
